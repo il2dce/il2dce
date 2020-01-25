67a94dc1a8077ef400ccc05aae668c681a193df4
il2dce <il2dce@googlemail.com>
2020-01-25 22:07:30 +0100

    Add Adlerangriff campaign (attack on airfields, radar, supply depots, trains, ships)

---
+++ IL2DCE/Campaigns/Adlerangriff/MissionSingle.cs
@@ -0,0 +1,23 @@
+﻿// IL2DCE: A dynamic campaign engine for IL-2 Sturmovik: Cliffs of Dover
+// Copyright (C) 2011 Stefan Rothdach
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+//$reference parts/IL2DCE/IL2DCE.Mission.dll
+//$debug
+
+public class Mission : IL2DCE.Mission.MissionSingle
+{
+
+}
